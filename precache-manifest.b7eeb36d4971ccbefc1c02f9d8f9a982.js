ee337879fea3919018a4ed90a817196100aa4e29
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2020-04-18 02:39:26 -0700

    Updates

---
+++ precache-manifest.b7eeb36d4971ccbefc1c02f9d8f9a982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fdc95cbc18877e2f30858ff0e0a0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af3999c6157bb09fa645",
+    "url": "/static/css/main.d3e4e03d.chunk.css"
+  },
+  {
+    "revision": "46778df48ec846669f41",
+    "url": "/static/js/2.cc971216.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc971216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3999c6157bb09fa645",
+    "url": "/static/js/main.bc0bffdd.chunk.js"
+  },
+  {
+    "revision": "cd53540fe2da6461f362",
+    "url": "/static/js/runtime-main.e8b7488b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chime-playground-frontend/precache-manifest.0e2175d7c7b88fc3ad0a32908e77cc56.js"
+  "/precache-manifest.b7eeb36d4971ccbefc1c02f9d8f9a982.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chime-playground-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
